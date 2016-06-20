8d3f47fc8149b5526b4f817b5e6cb63016a4c632
蒋磊 <1129173580@qq.com>
2016-06-20 10:53:46 +0800

    Initial Commit

---
+++ DataStorage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DataStorage
+//
+//  Created by jianglei on 16/6/20.
+//  Copyright © 2016年 xiaoshenyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
