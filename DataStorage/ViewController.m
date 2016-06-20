41f034663135fd1ab08caf29acbbd6d3069a86b9
蒋磊 <1129173580@qq.com>
2016-06-20 10:58:16 +0800

    添加了基础功能。

--- DataStorage/ViewController.m
+++ DataStorage/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>{
+
+    UITableView *_tableView;
+    NSMutableArray *_titleArray;
+}
 
 @end
 
@@ -17,6 +21,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.title =@"数据存储";
+    
+    _titleArray =[[NSMutableArray alloc]initWithObjects:@"对象序列化",@"Sqlite3", nil];
+    
+    _tableView =[[UITableView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStyleGrouped];
+    _tableView.dataSource =self;
+    _tableView.delegate =self;
+    [self.view addSubview:_tableView];
+    
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return _titleArray.count;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    return 60.0f;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    static NSString *identifierCell =@"Cell";
+    UITableViewCell *cell =[tableView dequeueReusableCellWithIdentifier:identifierCell];
+    if (!cell) {
+        
+        cell =[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifierCell];
+    }
+    cell.textLabel.text =_titleArray[indexPath.row];
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
