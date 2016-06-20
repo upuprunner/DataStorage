6ac5ced2b19d58e064e3f6be28ae4f6a484b7af5
蒋磊 <1129173580@qq.com>
2016-06-20 11:02:48 +0800

    sqlite部分。

---
+++ DataStorage/Car.h
@@ -0,0 +1,17 @@
+//
+//  Car.h
+//  DataBase
+//
+//  Created by jianglei on 16/6/19.
+//  Copyright © 2016年 xiaoshenyi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject
+
+@property (nonatomic,assign)int ID;
+@property (nonatomic,copy)NSString *name;
+@property (nonatomic,copy)NSString *price;
+
+@end
---
+++ DataStorage/Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  DataBase
+//
+//  Created by jianglei on 16/6/19.
+//  Copyright © 2016年 xiaoshenyi. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
